54fc35827997e64ac5c6188b460f7db7dce527db
was1paulina <palina@Palinas-MBP-2.sea.centerid.net>
2018-07-17 10:32:50 -0700

    Index page

---
+++ src/components/news_list_item.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NewsItem = (props) => {
+	return(
+		<div className="news_item">
+				<h3>{props.item.title}</h3>
+				<div>
+					{props.item.feed}
+				</div>
+		</div>
+		) 
+}
+
+export default NewsItem;
